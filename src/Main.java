73fef6ac13a089dad280dae59b4bcc40bb768af2
lexxxxa82 <lexxxxa.82@mail.ru>
2022-11-09 12:12:01 +0800

    выполнил дз

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[]weights ={90,91,93,92,85,87,88,89,0,0,0,0, };
+        int[]weights ={12,91,22,92,25,87,88,89,2,0,0,0, };
         weights[0] = 90;
         int januaryWeight = weights [0];
         System.out.println(januaryWeight);
